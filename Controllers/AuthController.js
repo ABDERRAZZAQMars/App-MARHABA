496ed9c1ac428bd0199f9e96998fe72c348a2672
mars <abderrazzaqmars99@gmail.com>
2022-10-13 09:26:00 +0100

    add initial files

---
+++ Controllers/AuthController.js
@@ -0,0 +1,37 @@
+// method : post
+// url : api/auth/login
+// acces : Public
+const Login = (req, res) => {
+    res.status(200).send(req.body)
+}
+
+// method : post
+// url : api/auth/register
+// acces : Public
+const Register = (req, res) => {
+    res.status(200).send('this a register function')
+}
+
+// method : post
+// url : api/auth/forgetpassword
+// acces : Public
+const ForgetPassword = (req, res) => {
+    res.status(200).send('this a Forget Password function')
+}
+
+// method : post
+// url : api/auth/resetpassword/:token
+// acces : Public
+const ResetPassword = (req, res) => {
+    // token = req.params.id
+    res.status(200).send('this a reset Password function of')
+}
+
+
+
+module.exports = {
+    Login,
+    Register,
+    ForgetPassword,
+    ResetPassword
+}
---
+++ Routes/AuthRoute.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+
+
+//Les Function Authentification
+const { Login, Register, ForgetPassword, ResetPassword } = require('../Controllers/AuthController')
+
+//url : /api/auth
+router.post('/login', Login)
+router.post('/register', Register)
+router.post('/forgetpassword', ForgetPassword)
+router.post('/resetpassword/:token', ResetPassword)
+
+
+
+module.exports = router
